5e2325a15710fb4db3924d71c981fbe9e51d8e23
PiketeroJunior <facundoromangimenez@gmail.com>
2025-06-05 20:29:03 -0300

    program builder

--- src/main/java/ar/edu/unahur/obj2/command/ProgramBuilder.java
+++ src/main/java/ar/edu/unahur/obj2/command/ProgramBuilder.java
@@ -1,5 +1,50 @@
 package ar.edu.unahur.obj2.command;
 
-public class ProgramBuilder {
+import java.util.ArrayList;
+import java.util.List;
 
+import ar.edu.unahur.obj2.command.comandos.Add;
+import ar.edu.unahur.obj2.command.comandos.Lod;
+import ar.edu.unahur.obj2.command.comandos.LodV;
+import ar.edu.unahur.obj2.command.comandos.Nop;
+import ar.edu.unahur.obj2.command.comandos.Operable;
+import ar.edu.unahur.obj2.command.comandos.Str;
+import ar.edu.unahur.obj2.command.comandos.Swap;
+
+public class ProgramBuilder {
+    private final List<Operable> instrucciones = new ArrayList<>();
+
+    public ProgramBuilder nop(){
+        instrucciones.add(new Nop());
+        return this;
+    }
+
+    public ProgramBuilder add(){
+        instrucciones.add(new Add());
+        return this;
+    }
+
+    public ProgramBuilder swap(){
+        instrucciones.add(new Swap());
+        return this;
+    }
+
+    public ProgramBuilder str(Integer direccion){
+        instrucciones.add(new Str(direccion));
+        return this;
+    }
+
+    public ProgramBuilder lod(Integer direccion){
+        instrucciones.add(new Lod(direccion));
+        return this;
+    }
+
+    public ProgramBuilder lodV(Integer valor){
+        instrucciones.add(new LodV(valor));
+        return this;
+    }
+
+    public List<Operable> build(){
+        return this.instrucciones;
+    }
 }
